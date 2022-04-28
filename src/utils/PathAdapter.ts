3793447767649efe9bee3c71c675f537680ac8f8
Jon Harding <jharding@ubriety.com>
2022-04-28 10:24:23 -0400

    Fix Safari regex issue

--- src/utils/PathAdapter.ts
+++ src/utils/PathAdapter.ts
@@ -2,7 +2,7 @@ export default class PathAdapter {
 
   public static kebabCase(path: string): string {
     let dataUrl = path
-    dataUrl = dataUrl.replace(/(?<=^\/|^)(?:en|fr)(?=\/|$)/, ''); // Strip language
+    dataUrl = dataUrl.replace(/(?:en|fr)(?=\/|$)/, ''); // Strip language
     dataUrl = dataUrl.replace(/^\/+|\/+$/g, ''); // Strip leading and trailing '/'
     dataUrl = dataUrl.replace(/\//g, '-');
     if(!dataUrl) {
