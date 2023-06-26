14f3c5290c4710460662f44266e65f2b80d03950
Jon Harding <jharding@ubriety.com>
2023-06-26 08:12:21 -0400

    Update FeedbackComponent

--- src/components/Feedback.vue
+++ src/components/Feedback.vue
@@ -2,26 +2,27 @@
   <div id="feedback" v-bind:class="{ active: feedbackVisible }">
     <div class="floating-actions">
       <button class="floating-button background-color-ui-primary"
-              :data-tip="t('app-give-feedback')"
-              :aria-label="t('app-give-feedback')"
+              :data-tip="giveFeedbackText"
+              :aria-label="giveFeedbackText"
               type="button"
               v-on:click="feedbackVisible = !feedbackVisible"
       >
         <span class=material-icons v-if="!feedbackVisible">
-          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-7 12h-2v-2h2v2zm0-4h-2V6h2v4z"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M424-320q0-81 14.5-116.5T500-514q41-36 62.5-62.5T584-637q0-41-27.5-68T480-732q-51 0-77.5 31T365-638l-103-44q21-64 77-111t141-47q105 0 161.5 58.5T698-641q0 50-21.5 85.5T609-475q-49 47-59.5 71.5T539-320H424Zm56 240q-33 0-56.5-23.5T400-160q0-33 23.5-56.5T480-240q33 0 56.5 23.5T560-160q0 33-23.5 56.5T480-80Z"/></svg>
         </span>
         <span class="material-icons" v-else>
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
         </span>
       </button>
     </div>
-    <div id="feedback-modal" :style="`height: ${height + modalHeaderHeight}px`">
+    <div id="feedback-modal">
       <div class="modal-header">
-        <div class="modal-header-heading">{{ t('app-give-feedback-heading') }}</div>
-        <div class="modal-header-message">{{ t('app-give-feedback-message') }}</div>
+        <div class="modal-header-heading">{{ giveFeedbackHeadingText }}</div>
+        <div class="modal-header-message">{{ giveFeedbackMessageText }}</div>
       </div>
       <div class="iframe-scroll-wrapper">
-        <iframe :style="`height: ${height}px`" :src="`${supportFormUrl}/${lang.langcode}/support`" scrolling="no" />
+        <iframe :style="`height: ${height}px`"
+          :src="`${supportFormUrl}/${lang.langcode}/support?product=mentor-connector`" scrolling="no" />
       </div>
     </div>
   </div>
@@ -35,7 +36,11 @@ export default {
   mixins: [BaseMixin],
 
   data() {
+    const lang = window.app.language.langcode;
     return {
+      giveFeedbackText: lang == 'fr' ? "Bienvenue, comment pouvons-nous vous aider aujourd'hui?" : "Welcome, how can we help you today?",
+      giveFeedbackHeadingText: lang == 'fr' ? "Bienvenue," : "Welcome,",
+      giveFeedbackMessageText: lang == 'fr' ? "comment pouvons-nous vous aider aujourd'hui?" : "how can we help you today?",
       email: '',
       feedbackVisible: false,
       message: '',
